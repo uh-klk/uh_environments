1088577a059c5e6426a0d2c21d67c09df5563e95
Nathan Burke <n.burke@natbur.com>
2015-10-06 18:19:59 +0100

    Update License

--- src/uh_simulation/webots/src/start_webots.cpp
+++ src/uh_simulation/webots/src/start_webots.cpp
@@ -1,43 +1,10 @@
 /*
  *  start_webots.cpp
  *
- *  Starts the Webots Simulator using a system() call, so Webots can be 
- *  started from a roslaunch file.
- *
- *  Date: September 2012
- *  Authors: David Butterworth
+ *  Date: October 2015
+ *  Authors: Nathan Burke
  *  
  */
-
-/*
- * Copyright (c) 2012, David Butterworth, KAIST
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <ros/ros.h>
 #include <iostream>
 #include <fstream>
@@ -48,53 +15,13 @@
 using std::string;
 using std::vector;
 
-/*
- Define Webots executable name.
- The full path will come from the PATH environment variable.
- Prefix with 'optirun' for Linux NVidia Bumblebee graphics driver
-
- Webots command line arguments:
- SYNOPSIS: webots [options] [worldfile]
- OPTIONS:
- --minimize                  minimize Webots window on startup
- --mode=<mode>               choose startup mode (overrides
- application preferences)
- argument <mode> must be one of:
- pause, realtime, run or fast
- (Webots PRO is required to use:
- --mode==run or --mode=fast)
- --help                      display this help message and exit
- --sysinfo                   display information of the system and
- exit
- --version                   display version information and exit
- --uuid                      display the UUID of the computer and exit
- --stdout                    redirect the controller stdout to the
- terminal
- --stderr                    redirect the controller stderr to the
- terminal
- --disable-modules-download  skip the check for module updates
- --force-modules-download    automatically download module updates
- (if any) at startupsystem
- --start-streaming-server    starts the Webots streaming server
- (Webots PRO is required)
- [="key[=value];..."]         parameters may be given as an option:
- port=1234 : starts the streaming
- server on port 1234
-
- See: http://www.cyberbotics.com/dvd/common/doc/webots/guide/section2.2.html
- */
-
-#define WEBOTS_EXECUTABLE "webots"
-
-//----------------------------------------------------------------------------//
-
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "webots");
 	int exitCode;
 
 	string webots_executable;
 	if (!ros::param::get("~webots", webots_executable)) {
-		webots_executable = WEBOTS_EXECUTABLE;
+		webots_executable = "webots";
 	}
 
 	string worldPath;
