30cf81a2a2b75efb34a840fbb4cfaeb380ea120f
Nathan Burke <n.burke@natbur.com>
2015-10-06 15:04:57 +0100

    Move sunflower environment simulations to uh_simulation

---
+++ src/uh_environments/README.md
@@ -0,0 +1,4 @@
+Sunflower Environments Package
+===
+
+Unified package for navigation environments for Sunflower, used by the ```map_server```
---
+++ src/uh_simulation/webots/src/start_webots.cpp
@@ -0,0 +1,120 @@
+/*
+ *  start_webots.cpp
+ *
+ *  Starts the Webots Simulator using a system() call, so Webots can be 
+ *  started from a roslaunch file.
+ *
+ *  Date: September 2012
+ *  Authors: David Butterworth
+ *  
+ */
+
+/*
+ * Copyright (c) 2012, David Butterworth, KAIST
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ros/ros.h"
+#include <iostream> // stringstream
+
+/*
+ Define Webots executable name.
+ The full path will come from the PATH environment variable.
+ Prefix with 'optirun' for Linux NVidia Bumblebee graphics driver
+
+ Webots command line arguments:
+	SYNOPSIS: webots [options] [worldfile]
+	OPTIONS:
+	  --minimize                  minimize Webots window on startup
+	  --mode=<mode>               choose startup mode (overrides
+								  application preferences)
+								  argument <mode> must be one of:
+								  pause, realtime, run or fast
+								  (Webots PRO is required to use:
+								  --mode==run or --mode=fast)
+	  --help                      display this help message and exit
+	  --sysinfo                   display information of the system and
+								  exit
+	  --version                   display version information and exit
+	  --uuid                      display the UUID of the computer and exit
+	  --stdout                    redirect the controller stdout to the
+								  terminal
+	  --stderr                    redirect the controller stderr to the
+								  terminal
+	  --disable-modules-download  skip the check for module updates
+	  --force-modules-download    automatically download module updates
+								  (if any) at startup
+	  --start-streaming-server    starts the Webots streaming server
+								  (Webots PRO is required)
+		[="key[=value];..."]         parameters may be given as an option:
+									   port=1234 : starts the streaming
+												   server on port 1234
+
+   See: http://www.cyberbotics.com/dvd/common/doc/webots/guide/section2.2.html
+ */
+
+#define WEBOTS_EXECUTABLE "webots"
+//#define WEBOTS_EXECUTABLE "optirun webots --mode=run"
+
+//----------------------------------------------------------------------------//
+
+int main(int argc, char **argv) {
+	// node name: webots
+	ros::init(argc, argv, "webots");
+
+	if (argc == 1) {
+		// no additional arguments, start Webots with previously opened world
+		ROS_INFO("Starting Webots simulator... \n");
+		if (system(WEBOTS_EXECUTABLE)) {
+		}
+	} else {
+		// additional arguments:
+		// argv[0] is full path to this ROS Node
+		// argv[1] is full path to world file
+		ROS_INFO("Starting Webots simulator... ");
+		ROS_INFO("Loading world: %s \n", argv[1]);
+		std::stringstream ss;
+		ss << WEBOTS_EXECUTABLE;
+
+		// Add any additional arguments
+		for(int i = 2; i < argc; i++) {
+			ss << " " << argv[i];
+		}
+
+		// Add world file
+		ss << " " << argv[1];
+
+		if (system(ss.str().c_str())) {
+		}
+	}
+
+	// No need to ros::spin()
+	// When we quit Webots, ROS will register
+	// that the webots node is shutting down.
+	return 0;
+}
+
